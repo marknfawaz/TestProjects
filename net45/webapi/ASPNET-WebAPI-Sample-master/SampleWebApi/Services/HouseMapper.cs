7f2cc8958bb1ed0032063d7c67a981df49565add
Mark Fawaz <markfawa@amazon.com>
2020-09-14 09:04:52 -0700

    Adding test projects

---
+++ net45/webapi/ASPNET-WebAPI-Sample-master/SampleWebApi/Services/HouseMapper.cs
@@ -0,0 +1,29 @@
+﻿using SampleWebApi.Models;
+
+namespace SampleWebApi.Services
+{
+    public class HouseMapper : IHouseMapper
+    {
+        public HouseDto MapToDto(HouseEntity houseEntity)
+        {
+            return new HouseDto()
+            {
+                Id = houseEntity.Id,
+                ZipCode = houseEntity.ZipCode,
+                City = houseEntity.City,
+                Street = houseEntity.Street
+            };
+        }
+
+        public HouseEntity MapToEntity(HouseDto houseDto)
+        {
+            return new HouseEntity()
+            {
+                Id = houseDto.Id,
+                ZipCode = houseDto.ZipCode,
+                City = houseDto.City,
+                Street = houseDto.Street
+            };
+        }
+    }
+}
---
+++ net45/webapi/ASPNET-WebAPI-Sample-master/SampleWebApi/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.OData.Extensions;
+using Microsoft.Owin;
+using Ninject;
+using Ninject.Web.Common;
+using Owin;
+using SampleWebApi.Models;
+using SampleWebApi.Repositories;
+using SampleWebApi.Services;
+using WebApiContrib.IoC.Ninject;
+
+[assembly: OwinStartup(typeof(SampleWebApi.Startup))]
+
+namespace SampleWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration
+            {
+                DependencyResolver = new NinjectResolver(CreateKernel())
+            };
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.AddODataQueryFilter();
+
+            app.UseWebApi(config);
+        }
+
+        public static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Bind<IHouseRepository>().ToConstant(new HouseRepository());
+            kernel.Bind<IHouseMapper>().To<HouseMapper>().InRequestScope();
+
+            return kernel;
+        }
+    }
+}
