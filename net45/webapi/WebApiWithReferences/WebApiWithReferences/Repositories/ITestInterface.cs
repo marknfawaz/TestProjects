ab891c654d7d2c2d26b9cdbf2c55a9b4a3dbe402
Mark Fawaz <markfawa@amazon.com>
2020-12-10 11:53:11 -0800

    Add sample interface file

---
+++ net45/webapi/WebApiWithReferences/WebApiWithReferences/Repositories/ITestInterface.cs
@@ -0,0 +1,16 @@
+﻿using SampleWebApi.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace SampleWebApi.Repositories
+{
+    [Description("Test")]
+    public interface ITestInterface
+    {
+        List<HouseEntity> GetAll();
+        HouseEntity GetSingle(int id);
+        HouseEntity Add(HouseEntity toAdd);
+        HouseEntity Update(HouseEntity toUpdate);
+        void Delete(int id);
+    }
+}
