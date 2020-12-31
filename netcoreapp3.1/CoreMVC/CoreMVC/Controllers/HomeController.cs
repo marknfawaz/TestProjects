8492d67e1a97824a1da18483a97178755cf2f2a7
Jon Louie <louiejon@amazon.com>
2020-12-30 19:54:15 -0800

    add asp.net core projects

---
+++ netcoreapp3.1/CoreMVC/CoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using CoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public override ViewComponentResult ViewComponent(string componentName)
+        {
+            return new ViewComponentResult
+            {
+                ViewComponentName = componentName,
+                Arguments = null,
+                ViewData = ViewData,
+                TempData = TempData
+            };
+        }
+    }
+}
