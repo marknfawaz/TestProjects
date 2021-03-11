7315c51a480ce46609a76a08c77eaf1737feee15
Mark Fawaz <markfawa@amazon.com>
2021-03-11 12:53:37 -0800

    Add an async web api return to improve testing

--- net45/webapi/WebApiWithReferences/WebApiWithReferences/Controllers/HouseController.cs
+++ net45/webapi/WebApiWithReferences/WebApiWithReferences/Controllers/HouseController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.OData;
@@ -27,7 +28,7 @@ namespace WebApiWithReferences.Controllers
         [HttpGet]
         [EnableQuery(PageSize = MaxPageSize)]
         [Route("")]
-        public IHttpActionResult Get(int page = 1, int pageSize = MaxPageSize)
+        public async Task<IHttpActionResult> Get(int page = 1, int pageSize = MaxPageSize)
         {
             if (pageSize > MaxPageSize)
             {
