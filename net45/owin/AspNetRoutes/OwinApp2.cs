0060505477d9ac4502f733f05e8bb54531c103a1
Jonathan Boudreault <72169598+Eruanion@users.noreply.github.com>
2021-05-06 10:34:56 -0700

    Add new apis for Cookies and QueryString (#23)

--- net45/owin/AspNetRoutes/OwinApp2.cs
+++ net45/owin/AspNetRoutes/OwinApp2.cs
@@ -8,6 +8,13 @@ namespace AspNetRoutes
         // Invoked once per request.
         public static Task Invoke(IOwinContext context)
         {
+            RequestCookieCollection cookies = context.Request.Cookies;
+            string owinCookieValue = cookies["OwinCookieKey"];
+
+            context.Request.QueryString = new QueryString("owin", "owinValue");
+            IReadableStringCollection stringquery = context.Request.Query;
+            string owinquery = stringquery["owin"];
+
             context.Response.ContentType = "text/plain";
             return context.Response.WriteAsync("Hello World 2");
         }
