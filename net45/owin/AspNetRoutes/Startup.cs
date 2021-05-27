76571239e07e762aab4cbe259e81c5340f342509
Jonathan Boudreault <72169598+Eruanion@users.noreply.github.com>
2021-05-26 19:08:27 -0700

    Owin path string (#28)
    * Add openid connect info
    
    * pathstring

--- net45/owin/AspNetRoutes/Startup.cs
+++ net45/owin/AspNetRoutes/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
+using System;
 
 namespace AspNetRoutes
 {
@@ -18,8 +19,12 @@ namespace AspNetRoutes
             ResponseCookieCollection cookies = context.Response.Cookies;
             cookies.Append("OwinCookieKey", "OwinCookieValue");
 
+            Uri.TryCreate(@"C:\Users", UriKind.Absolute, out Uri uriValue);
+            PathString path = PathString.FromUriComponent(uriValue);
+            bool check = path.Equals(new PathString(@"/C:/Users"));
+
             context.Response.ContentType = "text/plain";
-            return context.Response.WriteAsync("Hello World");
+            return context.Response.WriteAsync("Hello World " + check);
         }
     }
 }
