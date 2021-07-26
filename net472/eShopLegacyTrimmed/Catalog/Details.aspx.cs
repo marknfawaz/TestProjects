40a72e56f751ab7a6e4fe1faa59c08909a167cc3
Jonathan Boudreault <boudreaj@amazon.com>
2021-07-26 13:56:53 -0700

    Add trimmed eshop project

---
+++ net472/eShopLegacyTrimmed/Catalog/Details.aspx.cs
@@ -0,0 +1,22 @@
+﻿using eShopLegacyTrimmed.Models;
+using eShopLegacyTrimmed.Services;
+using System;
+using System.Web.UI;
+
+namespace eShopLegacyTrimmed.Catalog
+{
+    public partial class Details : Page
+    {
+        protected CatalogItem product;
+
+        public ICatalogService CatalogService { get; set; } = new CatalogServiceMock();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var productId = Convert.ToInt32(Page.RouteData.Values["id"]);
+            product = CatalogService.FindCatalogItem(productId);
+
+            this.DataBind();
+        }
+    }
+}
---
+++ net472/eShopLegacyTrimmed/Modules/eShopModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eShopLegacyTrimmed.Modules
+{
+    public class eShopModule : IHttpModule
+    {
+        public void Init(HttpApplication application)
+        {
+            application.EndRequest += (new EventHandler(Application_EndRequest));
+        }
+
+        private void Application_EndRequest(object source, EventArgs e)
+        {
+            // Do something with context near the end of request processing.
+        }
+
+        public void Dispose() { }
+    }
+}
