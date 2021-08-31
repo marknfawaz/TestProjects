6d8f3362a1f7588cdbe531f620c01097f2e231ee
Jonathan Boudreault <72169598+Eruanion@users.noreply.github.com>
2021-08-31 07:45:25 -0700

    Jb new owin solution (#47)
    * Add openid connect info
    
    * Add Microsoft.Owin.Security.IAuthenticationManager APIs
    
    * add owinparadise solution
    
    * New solution for new APIs

---
+++ net472/OwinExtraAPI/OwinExtraAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Authorization;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+
+namespace OwinExtraAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void OwinAuthorization(IAuthorizationRequirement req)
+        {
+            List<IAuthorizationRequirement> listReq = new List<IAuthorizationRequirement>() { req };
+            AuthorizationHandlerContext ahc = new AuthorizationHandlerContext(listReq, new ClaimsPrincipal(), null);
+            ahc.Succeed(req);
+
+            AuthorizationOptions aops = new AuthorizationOptions();
+            AuthorizationPolicy apol = new AuthorizationPolicy(listReq, new List<string>() { "" });
+            aops.AddPolicy("", apol);
+            aops.AddPolicy("", policy => { });
+
+            AuthorizationPolicyBuilder apolBuild = new AuthorizationPolicyBuilder(apol);
+            apolBuild = apolBuild.AddRequirements(req);
+        }
+    }
+}
