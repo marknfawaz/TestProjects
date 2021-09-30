678034b690c49dd9a42c380c8fbeb41b79c5d2cb
Jonathan Boudreault <72169598+Eruanion@users.noreply.github.com>
2021-09-30 15:21:13 -0700

    owin paradise update (#52)
    * Add openid connect info
    
    * Add Microsoft.Owin.Security.IAuthenticationManager APIs
    
    * add owinparadise solution
    
    * Add latest API

--- net472/OwinParadise/OwinParadise/AspNetRoutes.cs
+++ net472/OwinParadise/OwinParadise/AspNetRoutes.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.Owin;
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
 using Owin;
 using System;
+using Microsoft.Owin.Infrastructure;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataProtection;
+using Microsoft.Owin.Security.Infrastructure;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.AspNet.Identity.Owin;
+using System.Security.Claims;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols;
 
 namespace PortingParadise
 {
@@ -12,11 +22,11 @@ namespace PortingParadise
     {
         public void Configuration(IAppBuilder app)
         {
-            app.Run(Invoke);
+            app.Run(InvokeResponse);
         }
 
         // Invoked once per request.
-        public Task Invoke(IOwinContext context)
+        public Task InvokeResponse(IOwinContext context)
         {
             ResponseCookieCollection cookies = context.Response.Cookies;
             cookies.Append("OwinCookieKey", "OwinCookieValue");
@@ -27,6 +37,49 @@ namespace PortingParadise
 
             context.Response.ContentType = "text/plain";
             return context.Response.WriteAsync("Hello World " + check);
+        }
+
+        public static Task InvokeRequest(IOwinContext context)
+        {
+            RequestCookieCollection cookies = context.Request.Cookies;
+            string owinCookieValue = cookies["OwinCookieKey"];
+
+            IReadableStringCollection stringquery = context.Request.Query;
+            string owinquery = stringquery["owin"];
+
+            context.Response.ContentType = "text/plain";
+            return context.Response.WriteAsync("Hello World 2");
+        }
+
+        public async Task<bool> SignUpUser(IAuthenticationManager authenticationManager, ISecureDataFormat<AuthenticationTicket> df, AuthenticationTicket at)
+        {
+            List<ClaimsIdentity> claims = new List<ClaimsIdentity>() { new ClaimsIdentity() };
+            AuthenticationProperties authProp = new AuthenticationProperties();
+
+            var authResult = await authenticationManager.AuthenticateAsync("");
+            authenticationManager.Challenge(new string[] { "" });
+            var authResults = await authenticationManager.AuthenticateAsync(new string[] { "", "" });
+            authenticationManager.Challenge(authProp, new string[] { "" });
+            authenticationManager.SignOut(authProp, new string[] { "" });
+
+            string prot = df.Protect(at);
+            AuthenticationTicket unProtectedAT = df.Unprotect(prot);
+
+            return at.Equals(unProtectedAT);
+        }
+
+        public void Protector(IDataProtectionProvider protector)
+        {
+            string[] purposes = new string[] { "", "" };
+            IDataProtector prot1 = protector.Create(purposes);
+        }
+
+        public void UtilitiesLogger(ILogger logger, IAppBuilder app)
+        {
+            WebUtilities.AddQueryString("uri", new Dictionary<string, string>());
+            WebUtilities.AddQueryString("uri", "name", "value");
+
+            logger.WriteInformation("message");
         }
     }
 }
---
+++ net472/OwinParadise/OwinParadise/OwinExtraApi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Authorization;
+using Microsoft.Owin.Security.DataHandler;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using System.Security.Claims;
+
+namespace PortingParadise
+{
+    public class OwinExtraApi
+    {
+        public void OwinAuthorization(IAuthorizationRequirement req)
+        {
+            List<IAuthorizationRequirement> listReq = new List<IAuthorizationRequirement>() { req };
+            AuthorizationHandlerContext ahc = new AuthorizationHandlerContext(listReq, new ClaimsPrincipal(), null);
+            ahc.Succeed(req);
+
+            AuthorizationOptions aops = new AuthorizationOptions();
+            AuthorizationPolicy apol = new AuthorizationPolicy(listReq, new List<string>() { "" });
+            aops.AddPolicy("", apol);
+            aops.AddPolicy("", policy => { });
+
+            AuthorizationPolicyBuilder apolBuild = new AuthorizationPolicyBuilder(apol);
+            apolBuild = apolBuild.AddRequirements(req);
+        }
+
+        public void OwinDataHandler(SecureDataFormat<AuthenticationProperties> secure)
+        {
+            AuthenticationProperties props = new AuthenticationProperties();
+
+            string secured = secure.Protect(props);
+            AuthenticationProperties unsecured = secure.Unprotect(secured);
+
+            byte[] decoded = new byte[10];
+        }
+    }
+}
