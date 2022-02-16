cb5dfc0e7e2b42778f96b8237b7bbe752cd49264
Jon Louie <louiejon@amazon.com>
2022-02-15 16:29:16 -0800

    add rule contributions test project

---
+++ net48/RuleContributions/RuleContributions/RuleContributionsCore/Contributions/System.Linq.Dynamic.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Dynamic.Core;
+
+namespace RuleContributionsCore.Contributions
+{
+    public class DynamicQuery
+    {
+        private static void SampleDynamicLinqCall()
+        {
+            var list = new List<string>();
+            var dynamicQueryResult = list.AsQueryable()
+                .Where("Name.Contains(@0)", "SomeSubstring")
+                .ToList();
+        }
+    }
+}
