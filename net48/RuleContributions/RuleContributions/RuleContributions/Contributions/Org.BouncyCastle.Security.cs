08d9ce8cb7e58954cf1707b96abad2df3ca7681a
Jon Louie <louiejon@amazon.com>
2022-03-24 11:52:02 -0700

    add BouncyCastle usage

---
+++ net48/RuleContributions/RuleContributions/RuleContributions/Contributions/Org.BouncyCastle.Security.cs
@@ -0,0 +1,12 @@
+﻿using Org.BouncyCastle.Security;
+
+namespace RuleContributions.Contributions
+{
+    internal class BouncyCastleSecurity
+    {
+        private static void SampleBouncyCastleCrypto()
+        {
+            var random = new SecureRandom();
+        }
+    }
+}
---
+++ net48/RuleContributions/RuleContributions/RuleContributionsCore/Contributions/Org.BouncyCastle.Crypto.cs
@@ -0,0 +1,12 @@
+﻿using Org.BouncyCastle.Crypto;
+
+namespace RuleContributionsCore.Contributions
+{
+    internal class BouncyCastleCrypto
+    {
+        private static void SampleBouncyCastleCrypto()
+        {
+            AsymmetricCipherKeyPair keypair = null;
+        }
+    }
+}
