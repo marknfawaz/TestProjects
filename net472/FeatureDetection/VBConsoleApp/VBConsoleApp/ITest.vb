bdfbe9523a65a77716da63a8e2c17d68d95dc416
xueningl <xueningl@amazon.com>
2022-05-26 22:11:19 -0700

    add vb console solution

---
+++ net472/FeatureDetection/VBConsoleApp/VBConsoleApp/ITest.vb
@@ -0,0 +1,9 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+
+Namespace VBConsoleApp
+    Interface ITest
+        Function SayHello(ByVal hi As String) As String
+    End Interface
+End Namespace
---
+++ net472/FeatureDetection/VBConsoleApp/VBConsoleApp/TestClass.vb
@@ -0,0 +1,14 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+
+Namespace VBConsoleApp
+    Class TestClass
+        Implements ITest
+        Public Function SayHello(ByVal hi As String) As String Implements ITest.SayHello
+
+            Return "Saying hello to " & hi
+        End Function
+
+    End Class
+End Namespace
