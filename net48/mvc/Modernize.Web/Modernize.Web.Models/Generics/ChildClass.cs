fa178bee32cf4ffcae0c72faf111c0f33177b309
Mark Fawaz <markfawa@amazon.com>
2022-05-31 16:24:15 -0700

    Add generics with conditions

---
+++ net48/mvc/Modernize.Web/Modernize.Web.Models/Generics/ChildClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modernize.Web.Models.Generics
+{
+    public class ChildClass<ObjectType> : ParentClass<ObjectType> where ObjectType : class, IObjectType
+    {
+
+    }
+
+    public class ParentClass<T>
+    {
+
+    }
+
+    public class ObjectType : IObjectType
+    {
+
+    }
+
+    public interface IObjectType
+    {
+
+    }
+}
