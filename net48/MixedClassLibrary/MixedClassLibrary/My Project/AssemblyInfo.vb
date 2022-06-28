707595195c5ab7eacb94ead7d33234e51fa71bac
Chris Long <longachr@amazon.com>
2022-06-28 09:39:21 -0700

    add mixed solution test project

---
+++ net48/MixedClassLibrary/MixedClassLibrary/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MixedClassLibrary")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Amazon.com")>
+<Assembly: AssemblyProduct("MixedClassLibrary")>
+<Assembly: AssemblyCopyright("Copyright © Amazon.com 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ddc35f7a-e5a3-4058-898c-5edf05874d0d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ net48/MixedClassLibrary/MixedClassLibrary/VbClasses.vb
@@ -0,0 +1,107 @@
+﻿Imports System.ComponentModel.Design
+Imports System.Composition
+Imports System.Data.Entity
+Imports Microsoft.EntityFrameworkCore.Infrastructure
+Imports System.Reflection
+Imports System
+Imports System.IO, System.Collections
+Imports Newtonsoft.Json
+Imports System.Web
+Imports Microsoft.Owin.Security
+Imports Microsoft.Owin.Security.Infrastructure
+Imports Org.BouncyCastle.Security
+
+
+Namespace VbConsoleApp1
+
+    <System.Serializable()>
+    Public Class Person
+        Public Property Name As String
+        Public Property Age As Integer
+        Private Property Hobby As String
+
+        Public Shared _next As String = Nothing
+
+        Public Sub New(hobby As String)
+            hobby = hobby
+            Console.WriteLine("This is a constructor")
+        End Sub
+
+        Public Sub SetName(newName As String)
+            Name = newName
+        End Sub
+
+        Public Function GetName() As String
+            Console.WriteLine("Step in a function")
+            Dim _next As String = ""
+            Return Name
+        End Function
+
+        Public Async Function GetNameAsync() As Task(Of String)
+            Await Task.Delay(1)
+            Console.WriteLine(Me.Name)
+            Return "Boo"
+        End Function
+
+        Public Async Function Grade() As Task
+            Await GetNameAsync() : End Function
+
+        Protected Overridable Sub OnConfiguring(optionsBuilder As String)
+
+        End Sub
+
+        Function OwinSecurityInfrastructure(create As AuthenticationTokenCreateContext, receive As AuthenticationTokenReceiveContext) As String
+            Dim serializedTicket As String = create.SerializeTicket()
+            create.SetToken("")
+            receive.DeserializeTicket(serializedTicket)
+
+            Dim helper As SecurityHelper = New SecurityHelper()
+            helper.LookupChallenge("Authentication_Type", AuthenticationMode.Active)
+            Return ""
+        End Function
+
+    End Class
+
+    Class Book
+
+    End Class
+
+    <Serializable()>
+    Public Module BasicModule
+    End Module
+
+
+    Public Class Student : Inherits Person
+
+        Public Sub New()
+            MyBase.New("Studying")
+            Console.WriteLine(1)
+        End Sub
+
+        Public Property Gpa As Decimal
+
+        Protected Overrides Sub OnConfiguring(optionsBuilder As String)
+
+        End Sub
+
+
+    End Class
+
+    Public Interface IPerson
+        Sub Jump()
+        Function GetId() As Integer
+    End Interface
+
+    Public Class Customer
+        Implements IPerson
+
+        Public Sub Jump() Implements IPerson.Jump
+            Throw New NotImplementedException()
+        End Sub
+
+        Public Function GetId() As Integer Implements IPerson.GetId
+            Throw New NotImplementedException()
+        End Function
+    End Class
+
+End Namespace
