4528406c3a08000bdb1aa46ef1b7dc92847fe40d
Mark Fawaz <markfawa@amazon.com>
2022-07-13 12:49:41 -0700

    Add examplse of structs, enums, and records

---
+++ net48/mvc/Modernize.Web/Modernize.Web.Models/ValuesRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modernize.Web.Models
+{
+    public record ValuesRecord
+    {
+        public int Id;
+        public string Value;
+        public void MyMethod()
+        {
+            Console.Write("This method doesnt do anything");
+        }
+    }
+}
---
+++ net48/mvc/Modernize.Web/Modernize.Web.Mvc/UtilityClass.cs
@@ -0,0 +1,27 @@
+﻿using Modernize.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Modernize.Web.Mvc
+{
+    public class UtilityClass
+    {
+        public void MethodReferencingEnum()
+        {
+            var enumValue = ValuesEnum.Value1;
+            Console.WriteLine(enumValue);
+        }
+        public void MethodInvokingStructMethod()
+        {
+            var structInstance = new ValuesStruct();
+            structInstance.MyMethod();
+        }
+        public void MethodInvokingRecordMethod()
+        {
+            var recordInstance = new ValuesRecord();
+            recordInstance.MyMethod();
+        }
+    }
+}
