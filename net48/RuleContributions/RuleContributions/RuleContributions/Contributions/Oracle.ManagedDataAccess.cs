4ac78e6ba9a25525d838b8b962688c6964261640
Anushka Srinivasa <67922257+as14692@users.noreply.github.com>
2023-04-07 21:16:10 -0700

    Add Rules Contribution - Oracle.ManagedDataAccess (#94)
    Co-authored-by: Anushka Srinivasa <srinianu@amazon.com>

---
+++ net48/RuleContributions/RuleContributions/RuleContributions/Contributions/Oracle.ManagedDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using Oracle.ManagedDataAccess;
+
+namespace RuleContributions.Contributions
+{
+    internal class ManagedDataAccess
+    {
+        private static void SampleManagedDataAccess()
+        {
+            Oracle.ManagedDataAccess.Client.OracleConnection oc = new Oracle.ManagedDataAccess.Client.OracleConnection();
+        }
+    }
+}
