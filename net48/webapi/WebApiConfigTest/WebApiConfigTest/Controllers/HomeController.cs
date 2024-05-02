dd58ecac9ad1ac0660ace6866d1670201e334058
Samiullah Mohammed <samiull@amazon.com>
2024-05-02 16:32:35 -0700

    Add sample invocation of WebRequestHandler() for CTA rule
    Add dummy invocation of WebRequestHandler() to test porting.

--- net48/webapi/WebApiConfigTest/WebApiConfigTest/Controllers/HomeController.cs
+++ net48/webapi/WebApiConfigTest/WebApiConfigTest/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
 
 namespace WebApiConfigTest.Controllers
 {
@@ -11,6 +12,9 @@ namespace WebApiConfigTest.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+             // CTA: Test porting of WebRequestHandler
+            WebRequestHandler handler = new WebRequestHandler();
 
             return View();
         }
