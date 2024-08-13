88eee8b434d69d338a912ed65185927b883bd4cb
Mark Fawaz <66756186+marknfawaz@users.noreply.github.com>
2024-08-13 13:37:29 -0700

    Add projects with DLL references

---
+++ net48/ProjectsWithDLLs/ReferencedLibrary/ReferenceClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferencedLibrary
+{
+    public class ReferenceClass
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("ReferenceClass.DoSomething()");
+        }
+    }
+}
